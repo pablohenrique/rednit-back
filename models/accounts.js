bad8df7bb7022433126c468b6378719c35310b0f
pablohenrique <pablo.haoko@gmail.com>
2015-11-30 16:04:05 -0200

    Yuri dont go mad

--- models/accounts.js
+++ models/accounts.js
@@ -27,7 +27,7 @@ var accountsSchema = new mongoose.Schema({
             }
         },
         twitterAccount: {
-            twitterId: { type: Number, unique: true, required : true, dropDups: true,  index: true },
+            twitterId: { type: Number, unique: true, dropDups: true,  index: true },
             favorites: {
                 type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tweets' }],
                 index: true
